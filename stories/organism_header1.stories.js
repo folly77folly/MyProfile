bf479d79747e898c0dbda54dbba98903cc87915b
aqim <folly77folly@gmail.com>
2020-03-05 00:51:15 +0100

    Added Main header

--- stories/organism_header1.stories.js
+++ stories/organism_header1.stories.js
@@ -1,17 +1,25 @@
 import Header1 from '../src/molecules/header'
 import React from 'react'
-import { ThemeProvider } from "styled-components";
+import { StyledImage } from '../src/atoms/images/index'
+import image6 from '../src/atoms/images/image6.jpg'
 
 export default {
-    title: "Header1_molecule",
+    title: "organism_Header1",
     component: Header1,
   };
 
   export const Header1molecule = () => (
- 
+     
       <section className="bg-dark">
-        <Header1
-        />       
+      <div className="d-flex justify-content-around">
+      <Header1/>
+      <StyledImage
+       src = {image6}
+       className="rounded-circle"
+       height = "xxxlarge"
+       width = "xxxlarge"
+       />        
+      </div>
       </section>
     
   );
