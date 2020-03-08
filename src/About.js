f5dbd34a136cea1f2ac2983309ce24c9185a1bf4
aqim <folly77folly@gmail.com>
2020-03-08 02:05:17 +0100

    added about components

--- src/About.js
+++ src/About.js
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider } from "styled-components";
-import { StyledButttonNew, StyledButttonNew2 } from './atoms/Buttons/button'
 import Navlink from "./molecules/links";
 import StyledLogo from "./atoms/logo/index";
+import HeaderContent1 from './organisms/header-content'
+import HeaderSkill from './organisms/header-skills'
 
 const ThemeContext = React.createContext('light')
 const themes = {
@@ -21,7 +22,8 @@ function About() {
           <StyledLogo color="secondary" children="Personal"></StyledLogo>
           <Navlink />
         </div>
-        <div style={{marginTop:"8rem"}}><HeaderImage /></div>         
+        <div style={{marginTop:"8rem"}}><HeaderContent1 /></div>
+        <div style={{marginTop:"4rem"}}><HeaderSkill /></div>       
       </header>
     </div>      
     </ThemeProvider>  
