083c760642ada4e88ee1f07950e443493edeb43f
aqim <folly77folly@gmail.com>
2020-11-17 03:04:32 +0100

    corrections

--- src/organisms/footer-header.js
+++ src/organisms/footer-header.js
@@ -5,17 +5,16 @@ import image6 from "../atoms/images/image6.jpg";
 
 function FooterHeader() {
   return (
-    <div className="d-flex justify-content-center" style={{height:"25rem"}}>
-      <div className="col-md-5" style={{height:"50%"}}>
-        <div style={{height:"30%"}}></div>
-        <div style={{height:"30%"}}><FooterSide></FooterSide></div>
-        <div style={{height:"30%"}}></div>
+    <div className="d-flex justify-content-center" style={{ height: "25rem" }}>
+      <div className="col-md-5" style={{ height: "50%" }}>
+        <div style={{ height: "30%" }}></div>
+        <div style={{ height: "30%" }}>
+          <FooterSide></FooterSide>
+        </div>
+        <div style={{ height: "30%" }}></div>
       </div>
-      <div className="col-md-5" style={{height:"100%"}}>
-        <StyledImage
-          src={image6}
-          height="100%"
-        />
+      <div className="col-md-5" style={{ height: "100%" }}>
+        <StyledImage src={image6} height="100%" />
       </div>
     </div>
   );
